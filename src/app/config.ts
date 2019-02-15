0184271f462f3196fb71732b132a85410ebb2059
DeckerCHAN <DeckerCHAN@gmail.com>
2019-02-16 06:20:29 +1100

    Fix slash.

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,6 @@
 import {HttpHeaders} from '@angular/common/http';
 
-export const baseUrl = 'http://deckerchan.com/api';
+export const baseUrl = 'http://deckerchan.com/api/';
 
   export const antiCheatingUrl = 'http://localhost:20009/api/Analysis';
 export const headers = new HttpHeaders()
